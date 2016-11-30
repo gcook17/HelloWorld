55546362c55069bad733995b9343a2ab882f44ec
gcook17 <gcook17@comcast.net>
2016-11-30 09:58:35 -0800

    Initial file add.

---
+++ HelloWorld/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cmdSayIt = New System.Windows.Forms.Button()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.lblGreeting = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Your Name:"
+        '
+        'cmdSayIt
+        '
+        Me.cmdSayIt.Location = New System.Drawing.Point(103, 78)
+        Me.cmdSayIt.Name = "cmdSayIt"
+        Me.cmdSayIt.Size = New System.Drawing.Size(75, 23)
+        Me.cmdSayIt.TabIndex = 2
+        Me.cmdSayIt.Text = "Say Hello"
+        Me.cmdSayIt.UseVisualStyleBackColor = True
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(78, 26)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(153, 20)
+        Me.txtName.TabIndex = 3
+        '
+        'lblGreeting
+        '
+        Me.lblGreeting.Location = New System.Drawing.Point(12, 125)
+        Me.lblGreeting.Name = "lblGreeting"
+        Me.lblGreeting.Size = New System.Drawing.Size(249, 13)
+        Me.lblGreeting.TabIndex = 4
+        Me.lblGreeting.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.lblGreeting)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.cmdSayIt)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Hello World!"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents cmdSayIt As System.Windows.Forms.Button
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents lblGreeting As System.Windows.Forms.Label
+
+End Class
---
+++ HelloWorld/Form1.vb
@@ -0,0 +1,19 @@
+﻿Option Strict On
+
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        cmdSayIt.Enabled = False
+        txtName.Focus()
+    End Sub
+
+    Private Sub cmdSayIt_Click(sender As System.Object, e As System.EventArgs) Handles cmdSayIt.Click
+        lblGreeting.Text = String.Format("Hello {0}!", Trim(txtName.Text))
+    End Sub
+
+    Private Sub txtName_TextChanged(sender As System.Object, e As System.EventArgs) Handles txtName.TextChanged
+        cmdSayIt.Enabled = Not String.IsNullOrEmpty(txtName.Text) And Regex.Match(txtName.Text, "[^\s]").Success
+    End Sub
+End Class
