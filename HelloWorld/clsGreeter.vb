a30208d4f26beeb08464b671f88aafab358e3f6f
Greg Cook <gcook17@comcast.net>
2016-11-30 11:02:59 -0800

    Add new clsGreeter.

---
+++ HelloWorld/clsGreeter.vb
@@ -0,0 +1,15 @@
+﻿Option Strict On
+
+Public Class clsGreeter
+
+    Private mstrHiWord As String
+
+    Public Sub New()
+        mstrHiWord = "Hello"
+    End Sub
+
+    Public Function Greeting(vstrName As String) As String
+        Return mstrHiWord & " " & vstrName & "!"
+    End Function
+
+End Class
